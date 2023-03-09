327744559f410ef94af574da967610f316f14de0
asep <aseprendi487@gmail.com>
2023-03-09 20:53:51 +0700

    Homepage done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-crimson': '#f96e8f',
+      }
+    },
   },
   plugins: [],
 }
